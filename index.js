5f0ecba488041c8120172f30809aa373617eb39a
Prakash <pariyar360@gmail.com>
2023-03-29 19:18:42 +1100

    Email function added using emailjs

---
+++ index.js
@@ -0,0 +1,24 @@
+// send the contact message to me
+function contact(event) {
+    // stop auto refreshing because form auto refreshes
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
+    loading.classList += " modal__overlay--visible"
+    emailjs.sendForm (
+        "service_x02yxw8",
+        "template_petywfe",
+        event.target,
+        "EJB9nqNs1cdnkGghz"
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly. Here is my email address: pariyar360@gmail.com"
+        )
+    })
+}
+
+// Toggle modal when clicked
